a8f347f911d2fdfd3021610d28c4a3ae48a53fb7
jackpurcell <ljczmh@gmail.com>
2023-12-03 01:13:54 +0000

    update

---
+++ cal.py
@@ -0,0 +1,11 @@
+import json
+
+count = 0
+with open('/home/jkl6486/sok-llm-watermark/runs/xuandong23b/dipper.json', 'r') as f:
+    for line in f:
+        data = json.loads(line)
+        if data['attack_output_result']['prediction'] == True:
+            count += 1
+
+print(count)
+print(count/1000)
