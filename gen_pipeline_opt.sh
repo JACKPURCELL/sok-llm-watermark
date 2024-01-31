ee12394ad8e2112cfe6b683482b4e5c12a56f849
jackpurcell <ljczmh@gmail.com>
2024-01-31 02:14:11 -0500

    up shell

--- gen_pipeline_opt.sh
+++ gen_pipeline_opt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #===============================================================================
-# usecommand nohup bash gen_pipeline_opt.sh > gen_pipeline_opt.log 2>&1 &
+# usecommand nohup bash gen_pipeline_opt.sh > gen_pipeline_opt_v2.log 2>&1 &
 #===============================================================================
 
-watermark_types=("xuandong23b"	"rohith23"	"lean23"	"kiyoon23"	"xiaoniu23")
+watermark_types=("john23"	"xuandong23b"	"rohith23"	"lean23"	"aiwei23"	"kiyoon23"	"xiaoniu23")
 
-models=("opt")
+models=("opt" )
 gpus=("1,2,3")
 
 # cp_attack_types=("single-single" "triple-single")
@@ -16,9 +16,9 @@ echo "start $watermark_type"
     for model in "${models[@]}"; do
     echo "start $model"
         
-            # 对每个元素执行CUDA_VISIBLE_DEVICES="$gpus" python命令
-        CUDA_VISIBLE_DEVICES="$gpus" python watermark_reliability_release/generation_pipeline.py --overwrite True --watermark "$watermark_type" --wandb_project gen-c4-v1.0 --run_name gen-c4-"$watermark_type"-"$model"  --output_dir "$watermark_type"/c4/"$model" --min_generations 100 --max_new_tokens 200  --use_sampling False --num_beams 1 --model_name_or_path facebook/opt-1.3b
-        CUDA_VISIBLE_DEVICES="$gpus" nohup python watermark_reliability_release/evaluation_pipeline.py --overwrite_output_file True --watermark "$watermark_type" --wandb_project eva-c4-v1.0 --run_name eva-c4-"$watermark_type"-"$model"  --input_dir /home/jkl6486/sok-llm-watermark/runs/"$watermark_type"/c4/"$model" &
+        # 对每个元素执行CUDA_VISIBLE_DEVICES="$gpus" python命令
+        CUDA_VISIBLE_DEVICES="$gpus" python watermark_reliability_release/generation_pipeline.py --overwrite True --watermark "$watermark_type" --wandb_project gen-c4-v2.0-1000 --run_name gen-c4-"$watermark_type"-"$model"  --output_dir "$watermark_type"/c4/"$model" --min_generations 1000 --max_new_tokens 200 --use_sampling False --generation_batch_size 1 --num_beams 1 --model_name_or_path facebook/opt-1.3b
+        CUDA_VISIBLE_DEVICES="$gpus" nohup python watermark_reliability_release/evaluation_pipeline.py --overwrite_output_file True --watermark "$watermark_type" --wandb_project gen-c4-v2.0-1000 --run_name eva-c4-"$watermark_type"-"$model"  --input_dir /home/jkl6486/sok-llm-watermark/runs/"$watermark_type"/c4/"$model" &
 
 
     done
