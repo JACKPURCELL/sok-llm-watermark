cfc19a8b7c23a4150cd5fe961dbe85be7fe4f5c3
jackpurcell <ljczmh@gmail.com>
2024-03-10 03:35:15 -0400

    up

--- watermark_reliability_release/watermarks/xiaoniu23/xiaoniu23.py
+++ watermark_reliability_release/watermarks/xiaoniu23/xiaoniu23.py
@@ -176,12 +176,12 @@ class xiaoniu23_detector():
             watermarked = True
         else:
             watermarked = False
-        return {"best_sum_score": best_sum_score, 
+        return {"z_score": best_sum_score, 
                 "best_dist_q": best_dist_q, 
                 "prediction": watermarked}
     def dummy_detect(self, **kwargs):
         result = {
-                  "best_sum_score": 0.0,
+                  "z_score": 0.0,
                   "best_dist_q": 0.0,
                   "prediction": False}
         return result
